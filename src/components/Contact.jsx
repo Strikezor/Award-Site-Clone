0a5ed0d6e3ddf8652626658a2f5aaefa03eb9de0
Strikezor <akshaychaturvedi12345@gmail.com>
2024-11-30 13:24:24 +0530

    Adding files

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,9 @@ const ImageClipBox = ({ src, clipClass }) => (
 );
 
 const Contact = () => {
+  const Open = () => {
+    window.open("https://portfolio-three-psi-13.vercel.app/", "_blank");
+  };
   return (
     <div id="contact" className="my-20 min-h-96 w-screen  px-10">
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
@@ -37,13 +40,13 @@ const Contact = () => {
           <p className="mb-10 font-general text-[10px] uppercase">
             Join Zentry
           </p>
-
           <AnimatedTitle
             title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
             className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
           />
-
-          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
+          <div className="" onClick={Open}>
+            <Button title="contact us" containerClass="mt-10 cursor-pointer" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import { FaDiscord, FaLinkedin, FaGithub, FaUserCircle } from "react-icons/fa";
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
+  {
+    href: "https://portfolio-three-psi-13.vercel.app/",
+    icon: <FaUserCircle />,
+  },
+  { href: "https://discord.com/users/465186689896415243", icon: <FaDiscord /> },
+  // { href: "https://twitter.com", icon: <FaTwitter /> },
+  // { href: "https://youtube.com", icon: <FaYoutube /> },
+  // { href: "https://medium.com", icon: <FaMedium /> },
+  {
+    href: "https://www.linkedin.com/in/akshay-chaturvedi-1b1b3b1b1/",
+    icon: <FaLinkedin />,
+  },
+  {
+    href: "https://www.github.com/Strikezor",
+    icon: <FaGithub />,
+  },
 ];
 
 const Footer = () => {
@@ -12,10 +24,10 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          Re-Created by Akshay Chaturvedi
         </p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
+        <div className="flex justify-center gap-4  md:justify-start mr-[105px]">
           {socialLinks.map((link, index) => (
             <a
               key={index}
